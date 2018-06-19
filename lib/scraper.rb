8426ef552f431102b55ea1014bfc5ae1eaf91a48
iamdavidmichaelmoore <hey@iamdavidmoore.com>
2018-06-19 00:35:24 -0400

    Minor fix; defining scrapers still in progress

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,14 @@
 require_relative "./true_crime"
-# require 'open_uri'
-# require 'pry'
+require 'open-uri'
+require 'pry'
 
 
 class TrueCrimeScraper
-
   def self.scrape_categories(controller_base_path)
     categories_with_urls = []
     index_page = Nokogiri::HTML(open(controller_base_path))
-    index_page.css()each.with_index(1) do |category, num|
+    binding.pry
+    index_page.css().each.with_index(1) do |category, num|
       hash = {}
       hash[:category] = category.css().text
       hash[:url] = index_page.css()[num]["href"]
@@ -17,11 +17,9 @@ class TrueCrimeScraper
     categories_with_urls
   end
 
-  end
-
   # then I can call the Category class in order interpolate the names
   # as a concatenation with the BASE_PATH in order get the all of the docs
-  def self.scrape_documentaries(base_path_with_doc_url)
+  def self.scrape_documentaries(base_path_with_category_path)
 
   end
 
