c5c4b2805f1b616cf7acb401b46886239322bd6b
iamdavidmichaelmoore <hey@iamdavidmoore.com>
2018-06-23 17:38:27 -0400

    refactored code to facilitate the new sraper methods in the cli class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,30 +4,27 @@ require 'pry'
 
 
 class TrueCrimeScraper
-  def self.scrape_categories(controller_base_path)
-    categories_with_urls = []
-    index_page = Nokogiri::HTML(open(controller_base_path))
+  def self.scrape_urls(index_page_url)
+    docs_urls = []
+    index_page = Nokogiri::HTML(open(index_page_url))
     index_page.css("div.g1-column-inner #text-5 ul li").each_with_index do |category, num|
-      hash = {}
-      hash[:name] = category.text
-      hash[:url] = index_page.css("div.g1-column-inner #text-5 ul li a")[num]["href"]
-      categories_with_urls << hash
+      docs_urls << index_page.css("div.g1-column-inner #text-5 ul li a")[num]["href"]
     end
-    categories_with_urls
+    docs_urls
   end
 
-  def self.scrape_documentaries(category_url)
-    documentaries_with_attributes = []
-    category_page = Nokogiri::HTML(open(category_url))
+  def self.scrape_documentary_attributes(path)
+    attr_array = []
+    category_page = Nokogiri::HTML(open(path))
     category_page.css("figure.entry-featured-media").each_with_index do |documentary, num|
-      hash = {}
-      hash[:title] = category_page.css("h3.g1-delta.entry-title a")[num].text.gsub(/^?[( ]\d{4}[) ]/, " ").strip
-      hash[:year] = category_page.css("h3.g1-delta.entry-title a")[num].text.scan(/^?\d{4}/).last
-      hash[:category] = category_page.css("h1.g1-beta.g1-beta-2nd.archive-title").text
-      hash[:synopsis] = category_page.css("div.entry-body.g1-current-background div.entry-summary.g1-text-narrow p")[num].text
-      hash[:synopsis_url] = category_page.css("figure.entry-featured-media a")[num]["href"]
-      documentaries_with_attributes << hash
+      attr_hash = {}
+      attr_hash[:title] = category_page.css("h3.g1-delta.entry-title a")[num].text.gsub(/^?[( ]\d{4}[) ]/, " ").strip
+      attr_hash[:year] = category_page.css("h3.g1-delta.entry-title a")[num].text.scan(/^?\d{4}/).last
+      attr_hash[:category] = category_page.css("h1.g1-beta.g1-beta-2nd.archive-title").text
+      attr_hash[:synopsis] = category_page.css("div.entry-body.g1-current-background div.entry-summary.g1-text-narrow p")[num].text
+      attr_hash[:synopsis_url] = category_page.css("figure.entry-featured-media a")[num]["href"]
+      attr_array << attr_hash
     end
-    documentaries_with_attributes
+    attr_array
   end
 end
